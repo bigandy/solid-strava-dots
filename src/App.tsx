2363c0535941390366d8dbe129f95b22bf376258
Andrew Hudson <ahudson@scalable.com>
2024-01-29 18:31:04 +0000

    start on dot version of calendar

--- src/App.tsx
+++ src/App.tsx
@@ -1,101 +1,37 @@
 import type { Component } from "solid-js";
-import { createSignal, createEffect, onCleanup, Show } from "solid-js";
+
+import dayjs from "dayjs";
 
 import styles from "./App.module.css";
 
-import Thingulator from "./components/Thingulator";
 import Calendar from "./components/Calendar";
 
+const now = dayjs();
+const dummyData = {
+  nowAddOne: now.add(1, "day"),
+  now,
+  nowMinusOne: now.subtract(1, "day"),
+};
+
 const App: Component = () => {
-  const [showCalendar, setShowCalendar] = createSignal(true);
-
-  let timerId: number;
-
-  onCleanup(() => window.clearInterval(timerId));
-
-  const [count, setCount] = createSignal(0);
-  const [running, setRunning] = createSignal(false);
-
-  createEffect(() => {
-    let intervalId: number;
-    if (running()) {
-      intervalId = setInterval(() => {
-        setCount((c) => c + 1);
-      }, 100);
-    }
-    onCleanup(() => {
-      if (intervalId) {
-        clearInterval(intervalId);
-      }
-    });
-  });
-
-  const reset = () => {
-    setRunning(false);
-    setCount(0);
-  };
-
   return (
     <div class={styles.App}>
       <header class={styles.header}>
         <h1>Solid Strava Dots</h1>
       </header>
 
-      <button onClick={() => setRunning(true)}>Start</button>
-      <button onClick={() => setRunning(false)}>Stop</button>
-      <button onClick={reset}>Reset</button>
+      <h2>Regular Calendar with no data</h2>
+      <Calendar />
 
-      <Thingulator count={count} />
+      <h2>Calendar with dots</h2>
 
-      <button onClick={() => setShowCalendar(!showCalendar())}>
-        {showCalendar() ? "Hide" : "Show"} Calendar
-      </button>
-
-      {/* <Box showCalendar={showCalendar} setShowCalendar={setShowCalendar} /> */}
-      <Show when={showCalendar()}>
-        <Calendar />
-      </Show>
+      <Calendar showDots />
 
       {/* <Calendar initialOffset={-2} />
       <Calendar initialOffset={-1} />
       <Calendar initialOffset={1} />
 
       <Calendar initialOffset={2} /> */}
-    </div>
-  );
-};
-
-const Box = ({
-  showCalendar,
-  setShowCalendar,
-}: {
-  showCalendar: any;
-  setShowCalendar: any;
-}) => {
-  const handleBoxClose = () => {
-    console.log("close the box");
-    setShowCalendar((o) => !o);
-  };
-
-  return (
-    <div
-      style={{
-        "background-color": "red",
-        width: "100px",
-        height: "100px",
-      }}
-    >
-      {showCalendar() ? (
-        <>
-          <p>I am open</p>
-          <button onClick={handleBoxClose}>Close the box</button>
-        </>
-      ) : (
-        <>
-          <p>I am Closed</p>
-          <button onClick={handleBoxClose}>Open the box</button>
-        </>
-      )}
     </div>
   );
 };
--- src/components/Calendar/index.tsx
+++ src/components/Calendar/index.tsx
@@ -1,62 +1,10 @@
 import { type Component, For, createSignal, Show } from "solid-js";
-import dayjs from "dayjs";
 
 import Button from "../Button";
 
 import styles from "./styles.module.css";
 
-const dayToNumericDay = (firstDayofMonth: string) => {
-  let firstDayNumeric = 0;
-
-  const daysOfWeek = ["Mo", "Tu", "We", "Th", "Fr", "Sa", "Su"];
-
-  daysOfWeek.forEach((dayofWeek, index) => {
-    if (dayofWeek === firstDayofMonth) {
-      firstDayNumeric = index + 1;
-    }
-  });
-
-  return firstDayNumeric;
-};
-
-const getMonth = (offset: number) => {
-  const today = dayjs().add(offset, "month");
-  const month = today.format("MMMM");
-  const year = today.format("YYYY");
-  const daysInMonth = today.daysInMonth();
-  const firstDayofMonth = dayToNumericDay(today.startOf("month").format("dd"));
-
-  const todayDay = offset === 0 ? parseInt(today.format("DD")) : 0;
-
-  return {
-    month,
-    year,
-    daysInMonth,
-    firstDayofMonth,
-    todayDay,
-  };
-};
-
-const indexToDay = (index: number) => {
-  switch (index) {
-    case 1:
-      return "Monday";
-    case 2:
-      return "Tuesday";
-    case 3:
-      return "Wednesday";
-    case 4:
-      return "Thursday";
-    case 5:
-      return "Friday";
-    case 6:
-      return "Saturday";
-    case 7:
-      return "Sunday";
-    default:
-      return "no day";
-  }
-};
+import { getMonth, indexToDay } from "./utils";
 
 const Header = ({
   offset,
@@ -98,11 +46,51 @@ const Header = ({
   );
 };
 
-const Table: Component<{
-  offset: any;
-  setOffset: (offset: number) => void;
-}> = ({ offset, setOffset }) => {
-  const currentClass = () => (offset() === 0 ? styles.currentCalendar : "");
+const TableHeader: Component = () => {
+  return (
+    <thead>
+      <tr>
+        <th>
+          m<span class={styles.medium}>on</span>
+          <span class={styles.long}>day</span>
+        </th>
+        <th>
+          tu<span class={styles.medium}>e</span>
+          <span class={styles.long}>sday</span>
+        </th>
+        <th>
+          w<span class={styles.medium}>ed</span>
+          <span class={styles.long}>nesday</span>
+        </th>
+        <th>
+          th<span class={styles.medium}>u</span>
+          <span class={styles.long}>rsday</span>
+        </th>
+        <th>
+          f<span class={styles.medium}>ri</span>
+          <span class={styles.long}>day</span>
+        </th>
+        <th>
+          sa<span class={styles.medium}>t</span>
+          <span class={styles.long}>urday</span>
+        </th>
+        <th>
+          su<span class={styles.medium}>n</span>
+          <span class={styles.long}>day</span>
+        </th>
+      </tr>
+    </thead>
+  );
+};
+
+interface Props {
+  initialOffset?: number;
+  showDots?: boolean;
+  data?: any; // TODO: type this
+}
+
+const Calendar: Component<Props> = ({ initialOffset = 0, showDots, data }) => {
+  const [offset, setOffset] = createSignal(initialOffset);
 
   const rowsReturn = () => {
     // Rows
@@ -126,16 +114,22 @@ const Table: Component<{
                   dayCount++;
                   return (
                     <td
-                      class={`${
-                        dayCount === todayDay
-                          ? `${styles.calendarDay} ${styles.calendarDayActive}`
-                          : `${styles.calendarDay}`
-                      }`}
+                      classList={{
+                        [styles.calendarDay]: true,
+                        [styles.calendarDayActive]: dayCount === todayDay,
+                      }}
                     >
                       <span class={`${styles.dayName} vh`}>
                         {indexToDay(i() + 1)}
                       </span>
-                      <span class={styles.dayCount}>{dayCount}</span>
+                      <span
+                        classList={{
+                          [styles.dayCount]: true,
+                          vh: showDots,
+                        }}
+                      >
+                        {dayCount}
+                      </span>
                     </td>
                   );
                 }
@@ -148,58 +142,19 @@ const Table: Component<{
   };
 
   return (
-    <div class={`${styles.calendar} ${currentClass()}`}>
-      <Header offset={offset} setOffset={setOffset} showOffset={false} />
-      <table>
-        <thead>
-          <tr>
-            <th>
-              m<span class={styles.medium}>on</span>
-              <span class={styles.long}>day</span>
-            </th>
-            <th>
-              tu<span class={styles.medium}>e</span>
-              <span class={styles.long}>sday</span>
-            </th>
-            <th>
-              w<span class={styles.medium}>ed</span>
-              <span class={styles.long}>nesday</span>
-            </th>
-            <th>
-              th<span class={styles.medium}>u</span>
-              <span class={styles.long}>rsday</span>
-            </th>
-            <th>
-              f<span class={styles.medium}>ri</span>
-              <span class={styles.long}>day</span>
-            </th>
-            <th>
-              sa<span class={styles.medium}>t</span>
-              <span class={styles.long}>urday</span>
-            </th>
-            <th>
-              su<span class={styles.medium}>n</span>
-              <span class={styles.long}>day</span>
-            </th>
-          </tr>
-        </thead>
-        <tbody>{rowsReturn()}</tbody>
-      </table>
-    </div>
-  );
-};
-
-const Calendar: Component<{ initialOffset?: number }> = ({
-  initialOffset = 0,
-}) => {
-  const [offset, setOffset] = createSignal(initialOffset);
-
-  return (
-    <div>
-      <h1>Calendar</h1>
-
-      <div class={styles.wrapper}>
-        <Table offset={offset} setOffset={setOffset} />
+    <div class={styles.wrapper}>
+      <div
+        classList={{
+          [styles.calendar]: true,
+          [styles.showDots]: showDots,
+          [styles.currentCalendar]: offset() === 0,
+        }}
+      >
+        <Header offset={offset} setOffset={setOffset} showOffset={false} />
+        <table>
+          <TableHeader />
+          <tbody>{rowsReturn()}</tbody>
+        </table>
       </div>
     </div>
   );
