da9d78873b002ba8d6ca987c4b65d0799b9ca796
Andrew Hudson <ahudson@gmail.com>
2024-02-01 21:32:54 +0000

    move TableBody into own component file

---
+++ src/components/Calendar/TableBody.tsx
@@ -0,0 +1,61 @@
+import { type Component, For } from "solid-js";
+
+import styles from "./styles.module.css";
+
+import { getMonth, indexToDay } from "./utils";
+
+interface Props {
+  offset: any;
+  showDots?: boolean;
+}
+
+export const TableBody: Component<Props> = ({ offset, showDots }) => {
+  const rowsReturn = () => {
+    let dayCount = 0;
+    const { daysInMonth, firstDayofMonth, todayDay } = getMonth(offset());
+    let rows = Math.ceil((daysInMonth + firstDayofMonth) / 7);
+
+    return (
+      <For each={new Array(rows)}>
+        {(_, j) => (
+          <tr>
+            <For each={new Array(7)}>
+              {(_, i) => {
+                if (
+                  (j() === 0 && i() + 1 < firstDayofMonth) ||
+                  dayCount > daysInMonth - 1
+                ) {
+                  return <td></td>;
+                } else {
+                  dayCount++;
+                  return (
+                    <td
+                      classList={{
+                        [styles.calendarDay]: true,
+                        [styles.calendarDayActive]: dayCount === todayDay,
+                      }}
+                    >
+                      <span class={`${styles.dayName} vh`}>
+                        {indexToDay(i() + 1)}
+                      </span>
+                      <span
+                        classList={{
+                          [styles.dayCount]: true,
+                          vh: showDots,
+                        }}
+                      >
+                        {dayCount}
+                      </span>
+                    </td>
+                  );
+                }
+              }}
+            </For>
+          </tr>
+        )}
+      </For>
+    );
+  };
+
+  return <tbody>{rowsReturn()}</tbody>;
+};
--- src/components/Calendar/index.tsx
+++ src/components/Calendar/index.tsx
@@ -1,11 +1,10 @@
-import { type Component, For, createSignal } from "solid-js";
+import { type Component, createSignal } from "solid-js";
 
 import styles from "./styles.module.css";
 
-import { getMonth, indexToDay } from "./utils";
-
 import { Header } from "./Header";
 import { TableHeader } from "./TableHeader";
+import { TableBody } from "./TableBody";
 
 interface Props {
   initialOffset?: number;
@@ -14,55 +13,6 @@ interface Props {
 
 const Calendar: Component<Props> = ({ initialOffset = 0, showDots }) => {
   const [offset, setOffset] = createSignal(initialOffset);
-
-  const rowsReturn = () => {
-    // Rows
-    // calculate number of rows.
-    let dayCount = 0;
-    const { daysInMonth, firstDayofMonth, todayDay } = getMonth(offset());
-    let rows = Math.ceil((daysInMonth + firstDayofMonth) / 7);
-
-    return (
-      <For each={new Array(rows)}>
-        {(_, j) => (
-          <tr>
-            <For each={new Array(7)}>
-              {(_, i) => {
-                if (
-                  (j() === 0 && i() + 1 < firstDayofMonth) ||
-                  dayCount > daysInMonth - 1
-                ) {
-                  return <td></td>;
-                } else {
-                  dayCount++;
-                  return (
-                    <td
-                      classList={{
-                        [styles.calendarDay]: true,
-                        [styles.calendarDayActive]: dayCount === todayDay,
-                      }}
-                    >
-                      <span class={`${styles.dayName} vh`}>
-                        {indexToDay(i() + 1)}
-                      </span>
-                      <span
-                        classList={{
-                          [styles.dayCount]: true,
-                          vh: showDots,
-                        }}
-                      >
-                        {dayCount}
-                      </span>
-                    </td>
-                  );
-                }
-              }}
-            </For>
-          </tr>
-        )}
-      </For>
-    );
-  };
 
   return (
     <div class={styles.wrapper}>
@@ -76,7 +26,7 @@ const Calendar: Component<Props> = ({ initialOffset = 0, showDots }) => {
         <Header offset={offset} setOffset={setOffset} showOffset={false} />
         <table>
           <TableHeader />
-          <tbody>{rowsReturn()}</tbody>
+          <TableBody offset={offset} showDots={showDots} />
         </table>
       </div>
     </div>
